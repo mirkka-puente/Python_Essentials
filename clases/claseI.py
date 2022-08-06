6dba4e64818dc60cb122675d6a5dc142914203f7
kassette <50966326+mirkka-puente@users.noreply.github.com>
2022-08-06 00:31:08 -0500

    Add files via upload

---
+++ clases/claseI.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  4 20:01:40 2022
+
+@author: User
+"""
+class TheSimpleClass:
+    pass
+
+#instaciacion
+ob1 = TheSimpleClass()
+#constructor: definir las caracteristicas de una clase
+
+
