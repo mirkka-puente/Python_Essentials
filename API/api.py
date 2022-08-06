2be721a0973fb32be0185dac56ad5b3f5c4c18b5
kassette <50966326+mirkka-puente@users.noreply.github.com>
2022-08-06 00:30:06 -0500

    Add files via upload

---
+++ API/api.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  5 20:04:29 2022
+
+@author: User
+"""
+#API es una interface que conecta dos aplicaciones - intercambia informacion entre dos apps
+#el lenguaje de API debe ser JSON or XML
